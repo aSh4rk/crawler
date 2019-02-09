7a23acd5fa5f902adc8eb4f1dea2ecf805b3b535
Andrey Akulov <andreyshark@gmail.com>
2019-02-09 23:52:30 +0300

    Переделали реализацию клетки через композицию

--- crawler.rb
+++ crawler.rb
@@ -1,41 +1,63 @@
 class Cell
+
+	def initialize
+		@viewables = []
+	end
+
 	def getCoords
 	end
 
 	def is_empty?
+		@viewables.empty?
 	end
 
 	def render
-		" "
+		if is_empty?
+			" "
+		else	
+			@viewables.last.render
+		end
+	end
+
+	def put(viewable)
+		@viewables.push(viewable)
 	end
 end
 
-class Floor < Cell
+class Floor
 	def render
 		"•"
 	end
 end
 
-class VWall < Cell
+class VWall
 	def render
 		"‖"
 	end
 end
 
-class HWall < Cell
+class HWall
 	def render
 		"═"
 	end
 end
 
+empty_cell = Cell.new
+v_wall_cell = Cell.new
+h_wall_cell = Cell.new
+floor_cell = Cell.new
+
+h_wall_cell.put(HWall.new)
+v_wall_cell.put(VWall.new)
+floor_cell.put(Floor.new)
 
 map = [
-  HWall.new, # cell_h_wall
-  VWall.new, # cell_v_wall
-  Cell.new, # cell_empty
-  Floor.new, # cell_floor
+  h_wall_cell, # cell_h_wall
+  v_wall_cell, # cell_v_wall
+  empty_cell, # cell_empty
+  floor_cell, # cell_floor
 ]
 
 puts map
-  .map { |c| c.render}
+  .map { |c| c.render }
   .join('')
