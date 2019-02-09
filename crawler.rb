e650f5ef87c2b7a35038d7f0af90b04a4a6b26dc
Andrey Akulov <andreyshark@gmail.com>
2019-02-09 22:34:57 +0300

    Добавили реализацию отображения клети

--- crawler.rb
+++ crawler.rb
@@ -1,1 +1,41 @@
-puts "hello"
+class Cell
+	def getCoords
+	end
+
+	def is_empty?
+	end
+
+	def render
+		" "
+	end
+end
+
+class Floor < Cell
+	def render
+		"•"
+	end
+end
+
+class VWall < Cell
+	def render
+		"‖"
+	end
+end
+
+class HWall < Cell
+	def render
+		"═"
+	end
+end
+
+
+map = [
+  HWall.new, # cell_h_wall
+  VWall.new, # cell_v_wall
+  Cell.new, # cell_empty
+  Floor.new, # cell_floor
+]
+
+puts map
+  .map { |c| c.render}
+  .join('')
